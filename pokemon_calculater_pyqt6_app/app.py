fd0697ce2c7b187a91a8ef5c88f253e85b21c0b7
orionbh <mr.cleanexbh@gmail.com>
2023-06-06 13:05:02 -0700

    Added Fonts and Padding

--- pokemon_calculater_pyqt6_app/app.py
+++ pokemon_calculater_pyqt6_app/app.py
@@ -2,7 +2,7 @@ import sys
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QWidget, QHBoxLayout,
                              QVBoxLayout,QLabel,QComboBox, QPushButton,
                              )
-from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtGui import QPalette, QColor, QFont, QFontDatabase
 
 class MainWindow(QMainWindow):
 
@@ -16,12 +16,13 @@ class MainWindow(QMainWindow):
         right_pane = QVBoxLayout()
 
         title_label = QLabel("Pokemon Typing")
-        title_label.setStyleSheet("Qlabel { backround-color : red; }")
         left_pane.addWidget(title_label)
+        title_label.setFont(QFont("Impact", 30))
 
         self.combo = QComboBox()
         self.combo.addItems(["Fire", "Water", "Grass"])
         self.combo.currentTextChanged.connect(self.update_advantages)
+        self.combo.setFont(QFont("Impact", 18))
           
         left_pane.addWidget(self.combo)      
 
@@ -34,10 +35,12 @@ class MainWindow(QMainWindow):
         self.strength = QLabel("Strength Result")
         self.strength.adjustSize()
         right_pane.addWidget(self.strength)
+        self.strength.setFont(QFont("Impact", 17))
 
         self.weakness = QLabel("Weakness Result")
         self.weakness.adjustSize()
         right_pane.addWidget(self.weakness)
+        self.weakness.setFont(QFont("Impact", 17))
 
 
 
@@ -47,14 +50,14 @@ class MainWindow(QMainWindow):
 
         #display advantages and disadvantages
         if type == "Fire":
-            self.weakness.setText("Weak to Water ")
-            self.strength.setText("Strong to Grass ")
+            self.weakness.setText("2x Weak to Water ")
+            self.strength.setText("2x Strong to Grass ")
         if type == "Water":
-            self.weakness.setText("Weak to Grass ")
-            self.strength.setText("Strong to Fire ")
+            self.weakness.setText("2x Weak to Grass ")
+            self.strength.setText("2x Strong to Fire ")
         if type == "Grass":
-            self.weakness.setText("Weak to Fire ")
-            self.strength.setText("Strong to Water ") 
+            self.weakness.setText("2x Weak to Fire ")
+            self.strength.setText("2x Strong to Water ") 
     def get(self):
         print(self.weakness.text())
      
@@ -62,7 +65,7 @@ class MainWindow(QMainWindow):
         print(self.strength.text())
 
 app = QApplication(sys.argv)
-app.setStyleSheet("QWidget { font-size: 16px; background-color: red; color: White;}")
+app.setStyleSheet("QWidget { font-size: 16px; background-color: Red; color: white; padding: 10px 10px 10px 10px}")
 window = MainWindow()
 window.show()
 app.exec()
